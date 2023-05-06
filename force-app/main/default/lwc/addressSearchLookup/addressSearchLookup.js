98fbcb464fa40709df67923b8af73f6a3428f59f
Bensan George <bgeorge@repay.com>
2023-05-05 23:09:09 -0400

    Google Maps Validation integrated

--- force-app/main/default/lwc/addressSearchLookup/addressSearchLookup.js
+++ force-app/main/default/lwc/addressSearchLookup/addressSearchLookup.js
@@ -1,10 +1,29 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import lookupAddress from '@salesforce/apex/GMapsAddressLookup.lookupAddress';
 
 export default class AddressSearchLookup extends LightningElement {
+    standardAddress = '';
+    showStandardAddress = false;
+    addressLines = [];
+    
+    // This method is called when the user selects an address from the lookup
     handleAddressSearch(event) {
-        const addressSearchEvent = new CustomEvent('addresssearch', {
-            detail: event.detail
-        });
-        this.dispatchEvent(addressSearchEvent);
+        const address = this.template.querySelector('lightning-input-address');
+        // output all the event target values here
+        let addressLines = [address.street, address.city, address.province, address.postalCode, address.country];
+        this.addressLines = addressLines;
+    }
+
+    @wire(lookupAddress, {addressLines: '$addressLines'})
+    wiredAddress({error, data}) {
+        if(data){
+            this.showStandardAddress = true;
+            this.standardAddress = data;
+            this.error = undefined;
+        } else if(error) {
+            this.error = error;
+            this.standardAddress = undefined;
+            this.showStandardAddress = false;
+        }
     }
 }
