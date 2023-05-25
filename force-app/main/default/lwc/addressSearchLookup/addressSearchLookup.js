618637a1856892db57729186fb6942e9f475abe6
Bensan George <bgeorge@repay.com>
2023-05-25 09:48:05 -0400

    UI Records Update

--- force-app/main/default/lwc/addressSearchLookup/addressSearchLookup.js
+++ force-app/main/default/lwc/addressSearchLookup/addressSearchLookup.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, api } from 'lwc';
 import { getRecord, updateRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import lookupAddress from '@salesforce/apex/GMapsAddressLookup.lookupAddress';
 
 export default class AddressSearchLookup extends LightningElement {
@@ -22,8 +23,8 @@ export default class AddressSearchLookup extends LightningElement {
         if(data){
             this.showStandardAddress = true;
             this.standardAddress = data;
-            // updateRecord here
-            // updateRecordWithApiResponse(data);
+            console.log(this.recordId);
+            updateRecordWithApiResponse(data);
             this.error = undefined;
         } else if(error) {
             this.error = error;
@@ -35,5 +36,33 @@ export default class AddressSearchLookup extends LightningElement {
     updateRecordWithApiResponse(apiResponse) {
         const fields = [];
         fields['Id'] = this.recordId;
+        console.log('apiResponse', apiResponse);
+
+        fields['Shipping_Address__c'] = apiResponse.street;
+        fields['Shipping_City__c'] = apiResponse.city;
+        fields['Shipping_State__c'] = apiResponse.province;
+        fields['Shipping_PostalCode__c'] = apiResponse.postalCode;
+
+        const recordInput = { fields };
+
+        updateRecord(recordInput)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Address updated',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error updating record',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
     }
 }
